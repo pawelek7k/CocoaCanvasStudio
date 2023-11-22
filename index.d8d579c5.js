458d28b1c447589ced5e4ad9d6d531e40ea7d74c
pawelek7k <144838381+pawelek7k@users.noreply.github.com>
2023-11-22 17:01:33 +0000

    Deploying to gh-pages from @ pawelek7k/CocoaCanvasStudio@32b6904a905555fac6f8259afa220e3e0a179419 🚀

---
+++ index.d8d579c5.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-subscribe]"),closeModalBtn:document.querySelector("[data-modal-close-subscribe]"),modal:document.querySelector("[data-modal-subscribe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.d8d579c5.js.map
